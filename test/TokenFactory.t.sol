ca997b84a26f26c9eeef0738d6ce9cf55c47e02d
Scott Conner <scottconner@scottsttmachine.attlocal.net>
2024-09-09 20:57:13 -0500

    created foundry project

---
+++ test/TokenFactory.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {TokenFactory} from "../src/TokenFactory.sol";
+
+contract TokenFactoryTest is Test {
+    TokenFactory public facotry;
+
+    function setUp() public {
+    }
+
+    function test_Sanity() public {
+    }
+}
