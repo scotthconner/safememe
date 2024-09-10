1fa3d4ecce81155918cc939b0c4488ad4321ae31
Scott Conner <scottconner@scottsttmachine.attlocal.net>
2024-09-10 16:22:29 -0500

    Make the token initializeable

--- src/SafeToken.sol
+++ src/SafeToken.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// We are going to use a proxy-ready ERC20
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 /**
  * SafeToken
@@ -9,11 +11,21 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * A simple ERC20 token contract that mints a 1B supply token
  * and sends it back to the deployer.
  *
+ * While it looks like this contract is upgradeable, in practice it will
+ * not be. We will deploy this as an implementation contract, and then
+ * deploy a new instanced ERC1967 proxy for each individual safe token.
+ *
+ * The motivation behind this is to save chain-space and gas when
+ * launching a token.
+ *
  * The decimals will always use the default of 18. 
  */
-contract SafeToken is ERC20 {
+contract SafeToken is Initializable, ERC20Upgradeable {
     /**
-     * consructor 
+     * Initialize 
+     *
+     * This is called from a ERC1967 proxy deployment
+     * by the token factory.
      *
      * @param name   the name of the token
      * @param symbol the ticker of the token
@@ -31,7 +43,7 @@ contract SafeToken is ERC20 {
      * out of their wallet, reducing supply. Mainly, this is called by the token factory
      * to burn the supply that was retained in the factory.
      */
-    function burnMe() external returns (uint256) {
+    function burnMe() external {
         _burn(msg.sender, balanceOf(msg.sender)); 
     }
 }
