dc66ca96a579de8637fc60d7cd21e28ea1740dce
Scott Conner <scottconner@scottsttmachine.attlocal.net>
2024-09-09 21:06:39 -0500

    started documenting out the concept

--- src/TokenFactory.sol
+++ src/TokenFactory.sol
@@ -1,5 +1,31 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.23;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * TokenFactory
+ *
+ * This high level contract will be the entry point for the safe meme
+ * coin launcher. The process is as follows:
+ *
+ * 1) Coin developer supplies coin metadata, as well as any amount of initial liquidity
+ *    they'd like, with some reasonable minimum like .05 ETH.
+ * 2) In a single transaction, the coin is minted, with 50% of the supply added to LP along
+ *    with the ETH, and the other 50% of the supply contained within this factory.
+ * 3) Up to X minutes after launch, the dev can choose to: 
+ *              a) burn the 50% supply contained within this contract.
+ *              b) sell the 50% supply and burn all the proceeds as a way to kill snipers
+ * 4) After X minutes, anyone can "stomp" the supply and burn the funds.
+ *
+ * This has the following benefits:
+ *   - Tokens can be verified as immutable contracts that are safe, and not honey pots.
+ *   - Tokens immediately have locked liquidity and cannot be rugged.
+ *   - Tokens are available on Uniswap, and compatible with all front-ends, screeners, and bots.
+ *
+ * The goal is to strike a balance on the perceived safety of pump.fun type coin launchers,
+ * but get the immediate benefits of global liquidity with some sniper safeguards built in. 
+ */
 contract TokenFactory {
+
 }
